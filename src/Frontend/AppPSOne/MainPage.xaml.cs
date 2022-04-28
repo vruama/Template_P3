adefb8203da2c535528915609dadefe722c0e8a8
vanessa.barros <vanessa.barros@edu.fecap.br>
2022-04-28 20:09:39 -0300

    Commit tela inicial

---
+++ src/Frontend/AppPSOne/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppPSOne
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Proximo(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+
+
+    }
+}
---
+++ src/Frontend/AppPSOne/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPSOne
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Voltar(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+        private void Entrar(object sender, EventArgs e)
+        {
+            Application.Current.Properties["Name"] = Login.Text;
+            Navigation.PushAsync(new Page2());
+        }
+    }
+}
